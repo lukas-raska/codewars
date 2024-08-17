28efd8bdbf852a65c4e7d63eebdfcdc4160d990a
lukas-raska <luky.raska@gmail.com>
2024-08-17 06:12:38 +0200

    add factorial method

--- src/main/java/solutions/kyu8/WilsonPrimes/WilsonPrime.java
+++ src/main/java/solutions/kyu8/WilsonPrimes/WilsonPrime.java
@@ -1,9 +1,25 @@
 package solutions.kyu8.WilsonPrimes;
 
+import java.math.BigDecimal;
+
 public class WilsonPrime {
 
     public static void main(String[] args) {
 
 
+
+
+    }
+
+    private static BigDecimal factorial(long number) {
+        if (number < 0) {
+            throw new ArithmeticException(number + " is not a natural number. Factorial is not defined.");
+        }
+        if (number == 0) {
+            return BigDecimal.ONE;
+        }
+        return BigDecimal
+                .valueOf(number)
+                .multiply(factorial(number - 1));
     }
 }
